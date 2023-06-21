34821cc213d27437d15cd31e20561db1f4c6c3c2
Jorge Fernando Rodriguez <jorferr89@gmail.com>
2023-06-20 22:36:36 -0300

    validaciones (parcial)

--- apps/tareas/forms.py
+++ apps/tareas/forms.py
@@ -2,13 +2,31 @@ from cProfile import label
 from tkinter import Widget
 from tokenize import Number
 from django.forms import *
-from .models import *
+from .models import *  
+from django import forms
+from django.utils import timezone
+from datetime import date
 
 class TareaForm(ModelForm):
     class Meta:
         model = Tarea
         fields = ['nombre', 'descripcion', 'fecha_limite', 'prioridad']
         exclude = ['usuario']
+
+        error_messages = {
+            'nombre': {
+                'required': 'El campo Nombre es requerido.',
+            },
+            'descripcion': {
+                'required': 'El campo Descripción es requerido.',
+            },
+            'fecha_limite': {
+                'required': 'El campo Fecha Límite es requerido.',
+            },
+            'prioridad': {
+                'required': 'El campo Prioridad es requerido.',
+            },
+        }
 
         labels = {
             'nombre': 'Nombre',
@@ -43,3 +61,28 @@ class TareaForm(ModelForm):
             )
 
         }
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if ((len(nombre) < 5) or (len(nombre) > 100)):
+            raise forms.ValidationError('El Nombre debe tener entre 5 y 100 caracteres.')
+        return nombre
+
+    def clean_descripcion(self):
+        descripcion = self.cleaned_data.get('descripcion')
+        if ((len(descripcion) < 5) or (len(descripcion) > 100)):
+            raise forms.ValidationError('La Descripción debe tener al menos 5 caracteres.')
+        return descripcion
+
+    def clean_fecha_limite(self):
+        fecha_limite = self.cleaned_data.get('fecha_limite')
+        formato_esperado = '%Y-%m-%d'
+        fecha_actual = timezone.localdate()
+
+        fecha_limite_str = fecha_limite.strftime(formato_esperado)
+        if fecha_limite_str != self.data['fecha_limite']:
+            raise forms.ValidationError('El formato de fecha es incorrecto.')
+
+        if fecha_limite < fecha_actual:
+            raise forms.ValidationError('La Fecha Límite debe ser mayor a la fecha actual.')
+        return fecha_limite
--- apps/tareas/models.py
+++ apps/tareas/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from datetime import date
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
+alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Se aceptan solamente valores alfanuméricos.')
+
 # Prioridades
 class Prioridad(models.Model):
-    nombre = models.TextField(verbose_name='Nombre', unique=True)
+    nombre = models.TextField(verbose_name='Nombre', unique=True, validators=[alphanumeric])
     
     def __str__(self):
         return self.nombre
@@ -20,8 +23,8 @@ class Prioridad(models.Model):
 
 # Tareas
 class Tarea(models.Model):
-    nombre = models.TextField(verbose_name='Nombre')
-    descripcion = models.TextField(verbose_name='Descripcion')
+    nombre = models.TextField(verbose_name='Nombre', validators=[alphanumeric])
+    descripcion = models.TextField(verbose_name='Descripcion', validators=[alphanumeric])
     fecha_limite = models.DateField(validators=[MinValueValidator(limit_value=date.today)], verbose_name='Fecha Límite')
     terminada = models.BooleanField(default=False, verbose_name='Terminada')
     prioridad = models.ForeignKey(Prioridad, on_delete=models.CASCADE, verbose_name='Prioridad')
